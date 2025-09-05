2f3dcdcb9f239f0c7ec74e6614e9a0d455bb52e6
BIru <gilangbbe@gmail.com>
2025-09-05 11:55:29 +0700

    Add initial implementation of HealthCareCatalogView with placeholder content

---
+++ triage/Views/HealthCareCatalogView.swift
@@ -0,0 +1,18 @@
+//
+//  HealthCareCatalogView.swift
+//  triage
+//
+//  Created by Gilang Banyu Biru Erassunu on 05/09/25.
+//
+
+import SwiftUI
+
+struct HealthCareCatalogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthCareCatalogView()
+}
